fec8de0ad89ef244df8578b9041af78270c7aa2d
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-01-03 15:17:09 +0200

    25 current  2025-01-03 15:17:04  24.11.711615.1c6e20d41d6a  6.6.66                          *

--- home.nix
+++ home.nix
@@ -12,62 +12,60 @@ in {
     (import "${home-manager}/nixos")
   ];
 
-  programs.zsh = {
-    enable = true;
-    # opt for zsh-autcompletion rather than default
-    enableCompletion = false;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      g = "git";
-      d = "docker";
-      dcc = "docker compose";
-    };
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = ["git" "thefuck" "zoxide" "virtualenv"];
-      theme = "robbyrussell";
-    };
-
-    initExtra = ''
-      if [ -d "$HOME/bin" ] ; then
-          PATH="$HOME/bin:$PATH"
-      fi
-
-      if [ -d "$HOME/.local/bin" ] ; then
-          PATH="$HOME/.local/bin:$PATH"
-      fi
-
-      if [ -f "$HOME/.zsh_aliases" ]; then
-          . "$HOME/.zsh_aliases"
-      fi
-
-      if [ -f "$HOME/.zsh_functions" ]; then
-          . "$HOME/.zsh_functions"
-      fi
-
-      if [ -f "$HOME/.zsh_work" ]; then
-          . "$HOME/.zsh_work"
-      fi
-
-      if [ -f "$HOME/.env" ]; then
-          . "$HOME/.env"
-      fi
-    '';
-  };
-
+  home-manager.backupFileExtension = "baknix";
   home-manager.users.hermes = {
-    # This should be the same value as `system.stateVersion` in
     home.stateVersion = "24.11";
-
     home.file = {
       ".ssh/config" = {source = ./sources/ssh/config;};
       ".ssh/id_ed25519" = {source = ./sources/ssh/id_ed25519_servers;};
       ".ssh/id_ed25519.pub" = {source = ./sources/ssh/id_ed25519_servers.pub;};
       ".zsh_functions" = {source = ./sources/zsh/.zsh_functions;};
       ".zsh_aliases" = {source = ./sources/zsh/.zsh_aliases;};
+    };
+    programs.zsh = {
+      enable = true;
+      # opt for zsh-autcompletion rather than default
+      enableCompletion = false;
+      syntaxHighlighting.enable = true;
+
+      shellAliases = {
+        ll = "ls -l";
+        g = "git";
+        d = "docker";
+        dcc = "docker compose";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git" "thefuck" "zoxide" "virtualenv"];
+        theme = "robbyrussell";
+      };
+
+      initExtra = ''
+        if [ -d "$HOME/bin" ] ; then
+            PATH="$HOME/bin:$PATH"
+        fi
+
+        if [ -d "$HOME/.local/bin" ] ; then
+            PATH="$HOME/.local/bin:$PATH"
+        fi
+
+        if [ -f "$HOME/.zsh_aliases" ]; then
+            . "$HOME/.zsh_aliases"
+        fi
+
+        if [ -f "$HOME/.zsh_functions" ]; then
+            . "$HOME/.zsh_functions"
+        fi
+
+        if [ -f "$HOME/.zsh_work" ]; then
+            . "$HOME/.zsh_work"
+        fi
+
+        if [ -f "$HOME/.env" ]; then
+            . "$HOME/.env"
+        fi
+      '';
     };
   };
 }
