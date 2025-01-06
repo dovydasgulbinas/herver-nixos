67e7f83ffbe5052f29fd120a01ba83d43a91fef8
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-01-06 10:44:35 +0200

    change dns server

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
   hostname = "herver-nix"; # to alllow per-machine config
 in {
   networking.hostName = hostname;
+  networking.nameservers = ["1.1.1.1" "1.0.0.1"];
 
   imports = [
     # TODO: add if import if it does not exist use /etc/nixos conf
