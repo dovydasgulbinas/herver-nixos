169f5137bc9a91f4784d0c5e2f4a92b9934d7cfb
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-01-06 14:22:01 +0200

    remove helpers dir because its irrelevant

--- helpers/01_run_after_clean_install.sh
+++
@@ -1,8 +0,0 @@
-cd /etc/nixos
-sudo mkdir .git
-sudo chown hermes:users .git
-git config --global init.defaultBranch main
-git config --global user.name "Dovydas Gulbinas"
-git config --global user.email "dovydas.gulbinas@protonmail.com"
-git config --global --add safe.directory /etc/nixos
-git init
