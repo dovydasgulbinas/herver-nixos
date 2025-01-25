ed9f54b1b8fc9ebfb4fb3b9a598e6a35b64c87a7
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-01-25 14:12:36 +0200

    25 current  2025-01-25 14:12:31  24.11.713159.9c6b49aeac36  6.6.71                          *

--- configuration/herpc.nix
+++ configuration/herpc.nix
@@ -11,6 +11,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
   };
 
   # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
